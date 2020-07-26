190ac6cecebc71b3352207ebb50d30105da2d8b6
dantls <dan_tls@hotmail.com>
2020-07-26 04:02:18 -0400

    Finished

--- js/script.js
+++ js/script.js
@@ -1,14 +1,53 @@
 let globalUsers = [];
 let globalCountries = [];
 let globalUserContries = [];
+let globalFilteredUser = [];
 
-const start = async () => {
-  await fetchCountries();
-  await fetchUsers();
-  
+async function start() {
+
+  //"Normal"
+  // await fetchCountries();
+  // await fetchUsers();
+
+  //"Sequencial com medidor"
+  // console.time('promise');
+  // await promiseCountries();
+  // await promiseUsers();
+  // console.timeEnd('promise');
+
+  const p1 = promiseUsers();
+  const p2 = promiseCountries();
+  console.time('promise');
+  await Promise.all([p1,p2]);
+  console.timeEnd('promise');
+  hideSpinner();
   mergeUsersCountries();
   render();
+  filterUser();
 }
+
+function hideSpinner(){
+  const spinner = document.querySelector('.loader');
+  spinner.classList.add('displayNone');
+}
+
+function promiseUsers(){
+  return new Promise (async(resolve, reject) => {
+    const users = await fetchUsers();
+    setTimeout(() => {
+      resolve(users);      
+    }, 3000);
+  })
+}
+function promiseCountries(){
+  return new Promise (async(resolve, reject) => {
+    const countries = await fetchCountries();
+    setTimeout(() => {
+      resolve(countries);      
+    }, 3000);
+  })
+}
+
 
 async function fetchCountries() {
   const resource = await fetch('http://localhost:3001/countries');
@@ -29,7 +68,8 @@ async function fetchUsers() {
 
   const filteredUser = response.map(user =>{
     return({
-      name: user.name.first,
+      name: user.name.first+ ' ' + user.name.last,
+      nameLowerCase: (user.name.first+ ' ' + user.name.last).toLowerCase().trim(),
       avatar: user.picture.large,
       country: user.nat
     })
@@ -37,7 +77,7 @@ async function fetchUsers() {
   globalUsers = [...filteredUser];
 }
 
-const mergeUsersCountries =  () => {
+function mergeUsersCountries() {
   globalUserContries = globalUsers.map((user)=>{
     const country = globalCountries.find(country => country.country === user.country);
     return (
@@ -47,15 +87,18 @@ const mergeUsersCountries =  () => {
       }
     )
   });
+
+  globalUserContries.sort((a, b) => a.name.localeCompare(b.name));
+  globalFilteredUser = [...globalUserContries];
 }
 
-const render = () =>{
+function render () {
   const divCard = document.querySelector('.menu');
   divCard.innerHTML = `
-    ${globalUserContries.map(({name,avatar,countryFlag,countryName})=>{
+    ${globalFilteredUser.map(({name,avatar,countryFlag,countryName})=>{
       return(
         `<div class="card">
-          <img src="${avatar}" alt="countryName">
+          <img src="${avatar}" alt="${countryName}">
           <div class="info">
             <p>${name}</p>
             
@@ -70,21 +113,40 @@ const render = () =>{
               z-index: 5;
               overflow: hidden; " 
               class="flag"
-            > </div>
-            
-          </div>
-          
+            > </div>          
+          </div>  
         </div>`
       )
-    }).join('')}
+    }).join('')} 
+  `; 
+}
 
+function filterUser(){
+  const search = document.querySelector('#search');
+  const inputUser = document.querySelector('#userName');
   
-  `;
-  
+  inputUser.addEventListener('keyup',handleKeyUp);
+  search.addEventListener('click', handleButtonClick);
 
   
 }
 
+function handleKeyUp(event){
+  handleButtonClick();
+
+}
+
+
+function handleButtonClick(){
+  const inputUser = document.querySelector('#userName');
+  const valueInput = inputUser.value.toLowerCase().trim();
+ 
+  globalFilteredUser = globalUserContries.filter(user =>{
+    return user.nameLowerCase.includes(valueInput);
+  });
+  render();
+
+}
 
 
 start();
